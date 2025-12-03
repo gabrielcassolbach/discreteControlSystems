3ef838dbd1c6ce09ad7127356e1adfe9d6e41d24
gabrielbach <gabrielcassolbach@gmail.com>
2025-12-03 15:48:56 -0300

    skeleton with sensor

--- project/main.m
+++ project/main.m
@@ -1,14 +1,12 @@
-
-
-
-% Acionar motor no sentido horário e anti-horário.
-% parar motor.
-% ler sensor.
-
 microcontroller = arduino("COM7", "ESP32-WROOM-DevKitV1");
 
 addpath('./motor');
+addpath('./sensor');
+
 motor = setupMotor(microcontroller);
 [motor, dados] = moveMotor(motor, 90, 60); % arduino, angulo, velmax
 
+num_amostras = 10; % TO-DO
+taxa = 50; % TO-DO
 
+[tempo, luz] = readSensor(microcontroller,num_amostras,taxa);
--- project/sensor/readSensor.m
+++ project/sensor/readSensor.m
@@ -1,4 +1,17 @@
-function readSensor()
-    
+function [tempo,luz] = readSensor(a, numamostra,taxa)
+writePWMVoltage(a, 'D4', 0)
+tic;
+for i = 1:numamostra+1
+    while toc < taxa*i
+        luz(i) = readVoltage(a, "D15");
+    end
 end
-
+writePWMVoltage(a,'D4',3.3)
+for i = numamostra+1:(numamostra+1+numamostra+1)
+    while toc <taxa*i
+        luz(i) = readVoltage(a, "D15");
+    end
+end
+tempo = toc;
+writePWMVoltage(a, 'D4', 0)
+end
