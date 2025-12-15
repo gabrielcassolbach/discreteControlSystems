72501c4bc5cbe0836235e3a3832a4a2851571ae5
gabrielbach <gabrielcassolbach@gmail.com>
2025-12-15 15:29:38 -0300

    presentation version

--- project/main.m
+++ project/main.m
@@ -6,52 +6,47 @@ addpath('./sensor');
 num_amostras = 10; 
 taxa = 1; 
 motor = setupMotorDC();
-%[motor, dados] = moverParaPosicao(motor, -15, 30);
-%[tempo, luz, media] = readSensor(motor.arduino, num_amostras,taxa);
-%fprintf("Luz Medida: %d", sum(media) / numel(media));
 
+% calibra parâmetros
+% [motor, dados] = moverParaPosicao(motor, 45, 30);
+% [tempo, luz, media] = readSensor(motor.arduino, num_amostras,taxa);
+% fprintf("Luz Medida: %d", sum(media) / numel(media));
 
-ang_atual = 0;
+ang_atual = 90;
 
+% parâmetros para controlador proporcial da malha do controlador do sensor de luz.
+a = 0.8269;
+b = 0.0860;
+
+k = (a-b)/1; %cos(0) = 1.
+ 
 while(1)
-     [tempo, luz, media] = readSensor(motor.arduino, num_amostras,taxa);
+    [tempo, luz, media] = readSensor(motor.arduino, num_amostras,taxa); 
+    light_target = 0.3;
+    luz_medida = sum(media) / numel(media);
+ 
+    fprintf("Luz Medida: %d \n", luz_medida);
 
+    erro = light_target - luz_medida; 
+    angulo = acosd(cosd(ang_atual) - erro/k);
 
-     light_target = 0.01;
-     luz_medida = sum(media) / numel(media);
+    angulo_motor = angulo - ang_atual;
 
-     fprintf("Luz Medida: %d\n", luz_medida);
-
-     erro = light_target - luz_medida; 
-
-     k = 1.472;
-     a = 0.45;
-     b = 0.015;
-
-     v = (a-b)/k;
-
-     angulo = acosd(v);
-
-     fprintf("angulo: %d\n", angulo);
-
-     if(erro < 0)
-        angulo = -angulo;
-     end
-
-     if(abs(erro) > 0.1)
-        [motor, dados] = moverParaPosicao(motor, angulo, 30); % arduino, angulo, velmax
-        ang_atual = ang_atual + angulo;
-     end
-     pause(1);
+    fprintf("angulo_motor: %d \n", angulo_motor);
+    if(abs(erro) > 0.1 && ang_atual + angulo_motor <= 90)
+        [motor, dados] = moverParaPosicao(motor, angulo_motor, 30); arduino, angulo, velmax
+        ang_atual = ang_atual + angulo_motor;
+    end
+    fprintf("ang_atual: %d\n", ang_atual);
 end
-% 
+
 % Figura 1 - Erro
-%figure(1);
-%plot(dados.tempo, dados.erro);
-%xlabel('Tempo (s)');
-%ylabel('Erro (graus)');
-%grid on;
-%title('Erro x Tempo');
+figure(1);
+plot(dados.tempo, dados.erro);
+xlabel('Tempo (s)');
+ylabel('Erro (graus)');
+grid on;
+title('Erro x Tempo');
 
 % Figura 2 - Luz
 figure(2);
